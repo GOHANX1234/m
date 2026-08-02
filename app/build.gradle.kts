b95df3e7675327528d7b11b71c2be2e51515ef5d
GOHANX1234 <mohansuchil4@gmail.com>
2026-08-02 14:55:13 +0530

    Bump version code and version name to 4.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mna.streaming"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "3.0.0"
+        versionCode = 4
+        versionName = "4.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
